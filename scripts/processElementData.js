6a0922895e13937c910dec378b3c64c254850a65
Ricardo Carvalho <rafaelsoaresmd@gmail.com>
2021-07-28 18:36:12 -0300

    services-all

---
+++ scripts/processElementData.js
@@ -0,0 +1,62 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_g-qeke_BEeu1IYbz8vXXcA,_fz6GMO_CEeu1IYbz8vXXcA,_ir8DQe_CEeu1IYbz8vXXcAActivity"]="./../../ServicesAll - R.Raf/deliveryprocesses/resources/Implementação_701167EC_41c6ef74_Activity.jpeg"
+contentPage.processPage.imageFiles["_g-qeke_BEeu1IYbz8vXXcA,_fz6GMO_CEeu1IYbz8vXXcAActivity"]="./../../ServicesAll - R.Raf/deliveryprocesses/resources/Kanban_DA22C00A_ce105534_Activity.jpeg"
+contentPage.processPage.imageFiles["_g-qeke_BEeu1IYbz8vXXcAActivity"]="./../../ServicesAll - R.Raf/deliveryprocesses/resources/Processo_B0C0A8D6_b0c0a8d6_Activity.jpeg"
+contentPage.processPage.imageFiles["_g-qeke_BEeu1IYbz8vXXcA,_fz6GMO_CEeu1IYbz8vXXcA,_iFLlQe_CEeu1IYbz8vXXcAActivity"]="./../../ServicesAll - R.Raf/deliveryprocesses/resources/Desenvolvimento_810B3B54_52c0c2dc_Activity.jpeg"
+contentPage.processPage.imageFiles["_g-qeke_BEeu1IYbz8vXXcA,_fz6GMO_CEeu1IYbz8vXXcA,_hX8r0e_CEeu1IYbz8vXXcAActivity"]="./../../ServicesAll - R.Raf/deliveryprocesses/resources/Concepcão_3FD228E2_1187b06a_Activity.jpeg"
+contentPage.processPage.elementUrls["_eBnCIe_CEeu1IYbz8vXXcA"] = ["Analista Revisor","ServicesAll - R.Raf/capabilitypatterns/analista_revisor_E3363A91.html"];
+contentPage.processPage.elementUrls["_iFLlQe_CEeu1IYbz8vXXcA"] = ["Desenvolvimento","ServicesAll - R.Raf/deliveryprocesses/Desenvolvimento_810B3B54.html"];
+contentPage.processPage.elementUrls["_dshi8O_CEeu1IYbz8vXXcA"] = ["definir tabela de serviços e preços ","ServicesAll - R.Raf/capabilitypatterns/definir_tabela_de_servicos_e_precos__E32DE1EA.html"];
+contentPage.processPage.elementUrls["_eBnpMO_CEeu1IYbz8vXXcA"] = ["Documento de Visão Geral","ServicesAll - R.Raf/capabilitypatterns/documento_de_viso_geral_2DD7DDE4.html"];
+contentPage.processPage.elementUrls["_ZgDcIu-6Eeu1IYbz8vXXcA"] = ["documento de requisitos - refinado","ServicesAll - R.Raf/capabilitypatterns/documento_de_requisitos_-_refinado_E4DE320.html"];
+contentPage.processPage.elementUrls["_g-qeke_BEeu1IYbz8vXXcA"] = ["Processo","ServicesAll - R.Raf/deliveryprocesses/Processo_B0C0A8D6.html"];
+contentPage.processPage.elementUrls["_fz6GMO_CEeu1IYbz8vXXcA"] = ["Kanban","ServicesAll - R.Raf/deliveryprocesses/Kanban_DA22C00A.html"];
+contentPage.processPage.elementUrls["_T-9yJO-6Eeu1IYbz8vXXcA"] = ["documento de requisitos - refinado","ServicesAll - R.Raf/capabilitypatterns/documento_de_requisitos_-_refinado_908F02A6.html"];
+contentPage.processPage.elementUrls["_T-9LEe-6Eeu1IYbz8vXXcA"] = ["Analista de requisitos","ServicesAll - R.Raf/capabilitypatterns/analista_de_requisitos_815B7D34.html"];
+contentPage.processPage.elementUrls["_ZgDcJu-6Eeu1IYbz8vXXcA"] = ["sistema da plataforma de serviços feita","ServicesAll - R.Raf/capabilitypatterns/sistema_da_plataforma_de_servios_feita_D2DFC53F.html"];
+contentPage.processPage.elementUrls["_bNc4ce_CEeu1IYbz8vXXcA"] = ["Analista","ServicesAll - R.Raf/capabilitypatterns/analista_DCB19382.html"];
+contentPage.processPage.elementUrls["_T-9yIu-6Eeu1IYbz8vXXcA"] = ["Documento de Visão Geral","ServicesAll - R.Raf/capabilitypatterns/documento_de_viso_geral_C53414AD.html"];
+contentPage.processPage.elementUrls["_UbU-Ae-6Eeu1IYbz8vXXcA"] = ["Arquiteto de Sistema","ServicesAll - R.Raf/capabilitypatterns/arquiteto_de_sistema_FF561F11.html"];
+contentPage.processPage.elementUrls["_ZgDcJO-6Eeu1IYbz8vXXcA"] = ["Esquema de estrutura da plataforma e comunicação","ServicesAll - R.Raf/capabilitypatterns/esquema_de_estrutura_da_plataforma_e_comunicao_D9A8D119.html"];
+contentPage.processPage.elementUrls["_bNc4cO_CEeu1IYbz8vXXcA"] = ["Estabelecer as necessidades","ServicesAll - R.Raf/capabilitypatterns/estabelecer_as_necessidades_EE187F6C.html"];
+contentPage.processPage.elementUrls["_bNdfgu_CEeu1IYbz8vXXcA"] = ["Documento de necessidades de sistema","ServicesAll - R.Raf/capabilitypatterns/documento_de_necessidades_de_sistema_C7FEA1F.html"];
+contentPage.processPage.elementUrls["_T-9LEO-6Eeu1IYbz8vXXcA"] = ["Refinamento de requisitos","ServicesAll - R.Raf/capabilitypatterns/refinamento_de_requisitos_92C2691E.html"];
+contentPage.processPage.elementUrls["_UbU-AO-6Eeu1IYbz8vXXcA"] = ["Elaborar estrutura de equema de serviços e valor","ServicesAll - R.Raf/capabilitypatterns/elaborar_estrutura_de_equema_de_servicos_e_valor_10BD0AFB.html"];
+contentPage.processPage.elementUrls["_bNdfgO_CEeu1IYbz8vXXcA"] = ["Gerente","ServicesAll - R.Raf/capabilitypatterns/gerente_1348F5F9.html"];
+contentPage.processPage.elementUrls["_hX8r0e_CEeu1IYbz8vXXcA"] = ["Concepcão","ServicesAll - R.Raf/deliveryprocesses/Concepcão_3FD228E2.html"];
+contentPage.processPage.elementUrls["_ir8DQe_CEeu1IYbz8vXXcA"] = ["Implementação","ServicesAll - R.Raf/deliveryprocesses/Implementação_701167EC.html"];
+contentPage.processPage.elementUrls["_dshi8e_CEeu1IYbz8vXXcA"] = ["Necessidades do serviço","ServicesAll - R.Raf/capabilitypatterns/necessidades_do_servico_D1C6F600.html"];
+contentPage.processPage.elementUrls["_Z4Ndd--6Eeu1IYbz8vXXcA"] = ["Sistema da plateforma de serviços testada","ServicesAll - R.Raf/capabilitypatterns/sistema_da_plateforma_de_servios_testada_716E24E1.html"];
+contentPage.processPage.elementUrls["_bNc4c-_CEeu1IYbz8vXXcA"] = ["Cliente","ServicesAll - R.Raf/capabilitypatterns/cliente_3789034A.html"];
+contentPage.processPage.elementUrls["_bNdfhO_CEeu1IYbz8vXXcA"] = ["Necessidades de sistema","ServicesAll - R.Raf/capabilitypatterns/necessidades_de_sistema_D7DAD818.html"];
+contentPage.processPage.elementUrls["_dLVhce_CEeu1IYbz8vXXcA"] = ["Consultor","ServicesAll - R.Raf/capabilitypatterns/consultor_D3EF161F.html"];
+contentPage.processPage.elementUrls["_YE-roe_CEeu1IYbz8vXXcA"] = ["Concepcão","ServicesAll - R.Raf/capabilitypatterns/Concepcão_DA94708A.html"];
+contentPage.processPage.elementUrls["_UwJ-gO-6Eeu1IYbz8vXXcA"] = ["Elaborar a estrutura da plataformar","ServicesAll - R.Raf/capabilitypatterns/elaborar_a_estrutura_da_plataformar_52FC8915.html"];
+contentPage.processPage.elementUrls["_UbVlE--6Eeu1IYbz8vXXcA"] = ["Tabela de serviços e valor","ServicesAll - R.Raf/capabilitypatterns/tabela_de_servicos_e_valor_F104FC33.html"];
+contentPage.processPage.elementUrls["_OlYeUe-6Eeu1IYbz8vXXcA"] = ["Desenvolvimento","ServicesAll - R.Raf/capabilitypatterns/Desenvolvimento_4D5FBC81.html"];
+contentPage.processPage.elementUrls["_dLVhc-_CEeu1IYbz8vXXcA"] = ["Documento de requisitos","ServicesAll - R.Raf/capabilitypatterns/documento_de_requisitos_2EC685E7.html"];
+contentPage.processPage.elementUrls["_ZgDcIO-6Eeu1IYbz8vXXcA"] = ["Programador","ServicesAll - R.Raf/capabilitypatterns/programador_1516EEFA.html"];
+contentPage.processPage.elementUrls["_Z4NdcO-6Eeu1IYbz8vXXcA"] = ["Testar a implementação","ServicesAll - R.Raf/capabilitypatterns/testar_a_implementacao_636BBEE4.html"];
+contentPage.processPage.elementUrls["_Z4Ndc--6Eeu1IYbz8vXXcA"] = ["Consultor","ServicesAll - R.Raf/capabilitypatterns/consultor_ACDC42C2.html"];
+contentPage.processPage.elementUrls["_YMDdoe-6Eeu1IYbz8vXXcA"] = ["Implementação","ServicesAll - R.Raf/capabilitypatterns/Implementação_30F34BB2.html"];
+contentPage.processPage.elementUrls["_UbVlEe-6Eeu1IYbz8vXXcA"] = ["Necessidades do serviço","ServicesAll - R.Raf/capabilitypatterns/necessidades_do_servico_962D8C6B.html"];
+contentPage.processPage.elementUrls["_UbVlFe-6Eeu1IYbz8vXXcA"] = ["Esquema de estrutura da plataforma e comunicação","ServicesAll - R.Raf/capabilitypatterns/esquema_de_estrutura_da_plataforma_e_comunicao_5ABF6E8A.html"];
+contentPage.processPage.elementUrls["_Z4Ndce-6Eeu1IYbz8vXXcA"] = ["Testador","ServicesAll - R.Raf/capabilitypatterns/testador_5204D2FA.html"];
+contentPage.processPage.elementUrls["_dshi8-_CEeu1IYbz8vXXcA"] = ["Tabela de serviços e valor","ServicesAll - R.Raf/capabilitypatterns/tabela_de_servicos_e_valor_2C9E65C8.html"];
+contentPage.processPage.elementUrls["_eBnCIO_CEeu1IYbz8vXXcA"] = ["Visão geral do serviço","ServicesAll - R.Raf/capabilitypatterns/viso_geral_do_servico_F49D267B.html"];
+contentPage.processPage.elementUrls["_T-9yIO-6Eeu1IYbz8vXXcA"] = ["Documento de requisitos","ServicesAll - R.Raf/capabilitypatterns/documento_de_requisitos_CBFD2087.html"];
+contentPage.processPage.elementUrls["_ZgC1EO-6Eeu1IYbz8vXXcA"] = ["Implementar a estrutura da plataforma","ServicesAll - R.Raf/capabilitypatterns/implementar_a_estrutura_da_plataforma_EFE6786D.html"];
+contentPage.processPage.elementUrls["_Z4Ndde-6Eeu1IYbz8vXXcA"] = ["Gerente","ServicesAll - R.Raf/capabilitypatterns/gerente_1696B519.html"];
+contentPage.processPage.elementUrls["_dLVhcO_CEeu1IYbz8vXXcA"] = ["Documentar os requisitos","ServicesAll - R.Raf/capabilitypatterns/documentar_os_requisitos_E5560209.html"];
